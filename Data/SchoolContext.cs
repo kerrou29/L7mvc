bc3ce939dd354edd4499db44103e63c1a8336631
Khaoula <kerrou29@uw.edu>
2021-03-11 18:19:25 -0800

    comment

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿
+
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        //tinfo200:[2021-03-10-kerrou29-dykstra1]--Specifying the singular names in the tables in DBContext
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿namespace ContosoUniversity.Models
+{
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        //? means that the class is nullable
+        //null means that the grade is unknown and not assigned yet
+        public Grade? Grade { get; set; }
+        public Student Student { get; set; }
+
+
+
+    }
+}
